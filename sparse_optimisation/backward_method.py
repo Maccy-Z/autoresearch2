019b3b5413e352c6c7c52c15939674fc2bbccb44
Max Zhu <mz406@cam.ac.uk>
2026-06-20 04:02:52 +0100

    row_batch 20000 true single batch

--- sparse_optimisation/backward_method.py
+++ sparse_optimisation/backward_method.py
@@ -8,7 +8,7 @@ from sparse_utils import BitsparseTensor
 
 
 @torch_compile
-def AspB_block(A: Tensor, B_sparse: BitsparseTensor, row_batch=10000) -> Tensor:
+def AspB_block(A: Tensor, B_sparse: BitsparseTensor, row_batch=20000) -> Tensor:
     """ y = A @ B_sparse. Done blockwise to reduce peak vram usage.
         A.shape = [K, M]
         B.shape = [M, N]
