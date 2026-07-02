1f27da16ed2b6ce8472de9813688b53885ee164a
Max Zhu <mz406@cam.ac.uk>
2026-07-02 21:29:35 +0100

    Add nemotron tests

--- nemotron/start.py
+++ nemotron/start.py
@@ -1,5 +1,11 @@
 import os
+import sys
+
 os.environ["PYTORCH_ALLOC_CONF"] = "expandable_segments:True"
+
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if PROJECT_ROOT not in sys.path:
+    sys.path.append(PROJECT_ROOT)
 
 import torch
 from transformers import AutoTokenizer
